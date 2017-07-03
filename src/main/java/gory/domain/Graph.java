16cfc801edd032fbd87ae8ce0ed9614ff144ff14
Leonid Samokhine <lsamokhine@samokhine.com>
2017-07-03 11:05:57 -0500

    Additional stats for graphs

--- src/main/java/gory/domain/Graph.java
+++ src/main/java/gory/domain/Graph.java
@@ -1,14 +1,18 @@
 package gory.domain;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import gory.algorithm.BronKerbosch;
 import gory.service.OutputLogger;
 import lombok.Getter;
+import lombok.Setter;
 
 public class Graph {
-	@Getter
+	@Getter @Setter
 	private String name;
 
 	@Getter
@@ -65,19 +69,47 @@ public class Graph {
 		logger.writeLine("");
 
 		if(nodes.isEmpty()) return;
+
+		Map<Integer, AtomicInteger> nodeDegreeDistribution = new HashMap<>();
+		for(Node node : nodes) {
+			int degree = node.getConnectedNodes().size();
+			
+			AtomicInteger degreeCnt = nodeDegreeDistribution.get(degree);
+			if(degreeCnt == null) {
+				degreeCnt = new AtomicInteger();
+				nodeDegreeDistribution.put(degree, degreeCnt);
+			}
+			degreeCnt.incrementAndGet();
+		}
 		
 		int sumOfDegrees = 0;
 		logger.writeLine("Nodes:");
 		for(Node node : nodes) {
 			int degree = node.getConnectedNodes().size();
 			sumOfDegrees += degree;
-			logger.writeLine(node.toString()+(detailed ? " degree: "+degree : ""));
+			logger.writeLine(node.toString()+(detailed ? 
+					" degree: "+degree+", distribution: "+(1.0*nodeDegreeDistribution.get(degree).get()/getSize()) : 
+					""));
 		}
 		logger.writeLine("");
 
 		if(!detailed) return;
 		
-		for(Graph clique : findMaxCliques()) {
+		Set<Graph> cliques = findMaxCliques();
+		Map<Integer, AtomicInteger> cliqueSizeDistribution = new HashMap<>();
+		for(Graph clique : cliques) {
+			int size = clique.getSize();
+			
+			AtomicInteger cliqueSizeCnt = cliqueSizeDistribution.get(size);
+			if(cliqueSizeCnt == null) {
+				cliqueSizeCnt = new AtomicInteger();
+				cliqueSizeDistribution.put(size, cliqueSizeCnt);
+			}
+			cliqueSizeCnt.incrementAndGet();
+		}
+		
+		for(Graph clique : cliques) {
+			clique.setName(clique.getName()+" distribution: "+1.0*cliqueSizeDistribution.get(clique.getSize()).get()/cliques.size());
 			clique.log(logger, false);
 		}
 		
