97c3fd5fd2db0b6428838fc2812abcd35e915fa1
Leonid Samokhine <lsamokhine@samokhine.com>
2017-07-19 07:27:49 -0500

    Experiment 1

---
+++ src/main/java/experiment/Experiment1.java
@@ -0,0 +1,66 @@
+package experiment;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import gory.domain.Graph;
+import gory.domain.Node;
+import gory.domain.Partition;
+import gory.service.OutputLogger;
+import gory.service.PartitionBuilder;
+
+public class Experiment1 {
+	private int distance; // d
+	private int numberOfDigits; // m
+	
+	public void run() throws IOException {
+		readParameters();
+		
+		List<Integer> summands = new ArrayList<>();
+    	for(int i=1; i<=2*numberOfDigits-1; i=i+2) {
+    		summands.add(i);
+    	}
+    	
+    	Node node = new Node(new Partition(summands));
+    	Graph graph = new Graph(numberOfDigits*numberOfDigits+" - "+numberOfDigits+" graph", distance);
+    	graph.addNode(node);
+    	
+    	for(Partition partition : PartitionBuilder.build(numberOfDigits*numberOfDigits, numberOfDigits)) {
+    		if(node.getPartition().distanceTo(partition) != 1) continue;
+    		
+    		graph.addNode(new Node(partition));
+    	}
+
+    	OutputLogger out = new OutputLogger("output.txt");
+    	graph.log(out);
+    	out.close();
+	}
+	
+	private void readParameters() {
+		InputStream input = null;
+		try {
+			input = new FileInputStream("input.properties");
+
+			// load a properties file
+			Properties prop = new Properties();
+			prop.load(input);
+
+			numberOfDigits = Integer.valueOf(prop.getProperty("m"));
+			distance = Integer.valueOf(prop.getProperty("d"));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
--- src/main/java/gory/app/App.java
+++ src/main/java/gory/app/App.java
@@ -2,10 +2,12 @@ package gory.app;
 
 import java.io.IOException;
 
-import experiment.Experiment3;
+import experiment.Experiment1;
 
 public class App {
     public static void main(String[] args) throws IOException {
-    	Experiment3.run();
+    	Experiment1 experiment = new Experiment1();
+    	
+    	experiment.run();
     }
 }
