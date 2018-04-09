546814d37c17d5d2485387677343c6ed791af3f2
Leonid Samokhine <lsamokhine@samokhine.com>
2018-04-08 19:41:38 -0500

    Test commit

--- src/main/java/gory/experiment/BaseExperiment.java
+++ src/main/java/gory/experiment/BaseExperiment.java
@@ -140,6 +140,7 @@ public abstract class BaseExperiment implements Experiment {
 		}
 	}
 
+	
 	public void logNodesByCliques(Graph graph, Set<Graph> cliques, OutputLogger logger) {
 		logger.writeLine("Nodes by cliques:");
 		logger.writeLine("");
